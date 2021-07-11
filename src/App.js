8d61f557d2099abd377b36a8b38ee42bb0fa99c7
Justin Tubay <justin.tubay@gmail.com>
2021-07-11 00:42:04 -0400

    god fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img alt="Gods Will" src="http://newstatesman.com/sites/default/files/blogs_2014/11/2014_44_nature.jpg"></img>
+      <img className="god" alt="Gods Will" src="http://newstatesman.com/sites/default/files/blogs_2014/11/2014_44_nature.jpg"></img>
     </div>
   );
 }
